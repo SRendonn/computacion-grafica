d3548e562c1e2cf63ba3f0d3ccb2e45f95129092
Sebastian Rendon <sebasrg0505@gmail.com>
2022-03-28 16:49:09 -0500

    threejs tutorial scaffolding

---
+++ threejs-journey/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts"></script>
+
+<template>
+  <main>Hello ThreeJS Journey</main>
+</template>
+
+<style>
+html,
+body,
+#app {
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
